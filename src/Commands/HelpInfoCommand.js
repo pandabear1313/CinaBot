e34882e548fda4eb82cb224310a399282360b3cb
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-09 21:22:07 -0400

    test

--- src/Commands/HelpInfoCommand.js
+++ src/Commands/HelpInfoCommand.js
@@ -1,32 +1,56 @@
 /** @format */
 
+const paginationEmbed = require('discordjs-button-pagination');
+
 const Command = require("../Structures/Command.js");
 
-const Discord = require("discord.js");
-
-const{client, message, MessageEmbed } = require("discord.js");
-
-const { pagination } = require("reconlx");
-
+const { MessageEmbed , MessageButton} = require('discord.js');
 
 module.exports = new Command({
-	name: "helpinfo",
-	description: "More adv. help command",
+   name: "test",
+	description: "test",
 	permission: "SEND_MESSAGES",
-	run: async(message, args, client,) => {
-
-        const embed1 = new MessageEmbed().setTitle('one').setFooter('test')
-        const embed2 = new MessageEmbed().setTitle('two')
-
-        const embeds = [embed1, embed2];
-    
+	async run(message, args, client) {
 	
-        pagination({
-            embeds : embeds,
 
-            message : message,
+const embed1 = new MessageEmbed()
+                .setTitle('First Page')
+                .setDescription('This is the first page');
+ 
+const embed2 = new MessageEmbed()
+                .setTitle('Second Page')
+                .setDescription('This is the second page');
+ 
+const button1 = new MessageButton()
+                .setCustomId('previousbtn')
+                .setLabel('Previous')
+                .setStyle('DANGER');
+ 
+const button2 = new MessageButton()
+                .setCustomId('nextbtn')
+                .setLabel('Next')
+                .setStyle('SUCCESS');
+ 
+// Create an array of embeds
+pages = [
+    embed1,
+    embed2,
+    //....
+    //embedN
+];
+ 
+//create an array of buttons
+ 
+buttonList = [
+    button1,
+    button2
+]
+ 
+ 
+// Call the paginationEmbed method, first three arguments are required
+// timeout is the time till the reaction collectors are active, after this you can't change pages (in ms), defaults to 120000
+paginationEmbed(interaction, pages, buttonList, timeout);
+// There you go, now you have paged embeds
 
-            time: 4000,
-        })
-	}
+   }
 });
