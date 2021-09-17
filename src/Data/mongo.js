e2563ea3ad41ddfd8bfd02b5a536c6607a99f48e
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-17 18:38:09 -0400

    Mongo Connection

--- src/Data/mongo.js
+++ src/Data/mongo.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
-const { mongoPath } = require('./config.json')
+const { mongoPath } = require('../Data/config.json')
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+   // useFindAndModify: false,
   })
   return mongoose
 }
+
+mongoose.connection.on("connected", () => {
+
+console.log("connected to the database")
+})
