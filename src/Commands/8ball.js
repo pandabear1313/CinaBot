6a2449496f9a48809f232a62334e7bf66814124b
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-20 20:53:48 -0400

    8ball Added

--- src/Commands/8ball.js
+++ src/Commands/8ball.js
@@ -16,11 +16,25 @@ module.exports = new Command({
 
         if (!sayContent || !args[1])/* <--- both works*/ return message.channel.send(`<@${message.author.id}> Please send a message for me to predict your future :D`);
 
+        let user =
+        message.mentions.members.first() ||
+        message.guild.members.cache.get(args[0]) ||
+        message.member;
+
         if (!args[0]) return client.commands.get('help').execute(client, message, args, Discord)
         const n = Math.floor(Math.random() * eightBall.length)
         const embed = new Discord.MessageEmbed()
+
+        .setAuthor(`${message.author.username} Your Prediction`, message.author.displayAvatarURL())          
         .setColor("#00ff08")
-        .setDescription(`:8ball: ${eightBall[n]}`)
+        .addFields(
+            { name: 'Your Message <:boty:889655287408099388> ', value: `${sayContent}` },
+
+            { name: 'Your Answer  <:dev:889652696192286800> ', value: `${eightBall[n]}` },
+
+
+        )
+
         message.channel.send({ embeds: [embed] });
   
     }});
