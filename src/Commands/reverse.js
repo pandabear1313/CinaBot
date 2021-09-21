80a57cdc6d93fab144f810265724d8aaa313a87e
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-21 10:10:28 -0400

    Reverse Command

---
+++ src/Commands/reverse.js
@@ -0,0 +1,10 @@
+const Command  = require("../Structures/Command.js");
+
+module.exports = new Command({
+    name: "reverse",
+    description: "reverse your text!",
+    async run(message, args, client) {
+        if(!args.slice(1).length) return message.reply({content: ` Please supply some text!`})
+        message.reply({content: `${args.slice(1).join(" ").split("").reverse().join("")}`})
+    }
+});
