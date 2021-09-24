e103b070505a2d7e9ecacfff503bd4067d4e1935
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-23 23:43:53 -0400

    update

--- src/Commands/EchoCommand.js
+++ src/Commands/EchoCommand.js
@@ -2,6 +2,7 @@
 
 const Command = require("../Structures/Command");
 
+const Discord = require('discord.js')
 module.exports = new Command({
 
         name: "say",
@@ -9,9 +10,26 @@ module.exports = new Command({
         permission: "SEND_MESSAGES",
         async run(message, args, client) {
 
+            let user =
+            message.mentions.members.first() ||
+            message.guild.members.cache.get(args[0]) ||
+            message.member;
+    
+            const report = new Discord.MessageEmbed()
+    
+        .setAuthor(`${user.user.username} `, user.user.displayAvatarURL())
+        .setColor("#00FF1E")
+        .setTitle('Invaild Use of Command')
+        .addFields(
+            { name: 'Command', value: 'Say Command allows a user to send a message \nand bot would repeat that same message'},
+    
+            { name: 'Command Useage', value: 'Example  `!say` Hello world' }
+        )
+        .setTimestamp()  
+    
             const sayContent = args.slice(1).join(" ");
 
-            if (!sayContent || !args[1])/* <--- both works*/ return message.channel.send(`<@${message.author.id}> Send a message to let me say something`);
+            if (!sayContent || !args[1])  return message.channel.send({ embeds: [report] });
 
             const msg = await message.channel.send(sayContent);
 
