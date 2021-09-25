3cfdb704aee47e3c5e25e8d46ab83fa82725829e
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-25 16:45:58 -0400

    Update

--- src/Commands/DropdownMenu.js
+++ src/Commands/DropdownMenu.js
@@ -10,7 +10,7 @@ const { Collection } = require("mongoose");
 
 const Command = require("../Structures/Command.js");
 module.exports = new Command({
-	name: "p",
+	name: "f",
 	description: "Hello!",
 	permission: "SEND_MESSAGES",
 	async run(message, args, client) {
@@ -52,13 +52,29 @@ module.exports = new Command({
 
         )
 
+        let user =
+        message.mentions.members.first() ||
+        message.guild.members.cache.get(args[0]) ||
+        message.member;
+
+
         let embed = new Discord.MessageEmbed()
 
         embed
         
-        .setTitle("Welcome to the dropdown selection")
-        .setDescription("Choose the option you want to select")
-        .setColor("RANDOM")
+        .setAuthor( user.user.username, user.user.displayAvatarURL())
+        .setColor("#00FF1E")
+        .setTitle('CinaBot Help Info Page')
+        .addFields(
+            { name: 'Information', value: 'Welcome to** CinnaBot** help info page where you can find \nall the available commands that the bot could perform.'},
+    
+            { name: 'More Info', value: 'For More Information about **each** command \ntry using the `!helpinfo` Command'},
+    
+            { name: 'Default Prefix', value: '`!help`'},
+        )
+        .setTimestamp() 
+        .setThumbnail(user.user.displayAvatarURL())
+        .setImage("")
 
         let sendmsg = await message.channel.send({content : 
             " ", ephemeral:true, embeds : [embed], 
@@ -69,30 +85,70 @@ module.exports = new Command({
         let embed1 = new Discord.MessageEmbed()
 
         
-        .setTitle("You selected something!")
-        .setDescription("You chouse option 1")
-        .setColor("RANDOM")
+        .setAuthor( user.user.username, user.user.displayAvatarURL())
+        .setColor("#00FF1E")
+        .setTitle('**Welcome** to the `Information` section')
+        .addFields(
+            { name: 'Information Page', value: '**Information** Commands generally show information \nabout the bot as well as show statistics on users'},
+    
+            { name: 'Commands', value: '`ping` | `embed` | `userinfo` | `Botinfo` | `uptime` | `report` | `help` | `helpinfo`'},
+    
+            { name: 'More Information ', value: 'For more **Information** please check out the `!helpinfo` Command'},
+        )
+        .setTimestamp() 
+        .setThumbnail(user.user.displayAvatarURL())
+        .setImage("")
 
         let embed2 = new Discord.MessageEmbed()
 
         
-        .setTitle("You selected something!")
-        .setDescription("You chouse option 2")
-        .setColor("RANDOM")
+    .setAuthor( user.user.username, user.user.displayAvatarURL())
+    .setColor("#ff9100")
+	.setTitle('**Welcome** to the `Fun` section')
+    .addFields(
+        { name: 'Fun Page', value: '**Fun** Commands are used to `Troll/ have a good time` with your friends'},
+
+        { name: 'Commands', value: '`meme` | `fire` | `say` | `status` | `hello` | `hug`  | `pat`  | `ttc`  | `dog`  | `cat` | `8ball` | `dumbrate` | `hack` | `pickup` | `reverse` | `rps` | `ship` | `tweet` | '},
+
+        { name: 'More Information ', value: 'For more **Information** please check out the `!helpinfo` Command'},
+    )
+    .setTimestamp() 
+    .setThumbnail(user.user.displayAvatarURL())
+    .setImage("")
 
         let embed3 = new Discord.MessageEmbed()
 
-        
-        .setTitle("You selected something!")
-        .setDescription("You chouse option 3")
-        .setColor("RANDOM")
-
+        .setAuthor( user.user.username, user.user.displayAvatarURL())
+        .setColor("#00c8ff")
+        .setTitle('**Welcome** to the `Moderation` section')
+        .addFields(
+            { name: 'Moderation Page', value: '**Moderation** Commands are intentionally used to keep your server **Safe**'},
+    
+            { name: 'Commands', value: '`clear` | `setprefix`'},
+    
+            { name: 'Built-in Events', value: '`Anti-Swear system` | `AI Chat` | `Mod-Logs`'},
+    
+            { name: 'More Information ', value: 'For more **Information** please check out the `!helpinfo` Command'},
+        )
+        .setTimestamp() 
+        .setThumbnail(user.user.displayAvatarURL())
+        .setImage("")
+    
         let embed4 = new Discord.MessageEmbed()
 
-        
-        .setTitle("You selected something!")
-        .setDescription("You chouse option 4")
-        .setColor("RANDOM")
+        .setAuthor( user.user.username, user.user.displayAvatarURL())
+    .setColor("#00c8ff")
+	.setTitle('**Welcome** to the `Anime` section')
+    .addFields(
+        { name: 'Anime Page', value: '**Moderation** Commands are intentionally used to keep your server **Safe**'},
+
+        { name: 'Commands', value: '`wave` | `waifu` | `hug` | `pat` | `cuddle`'},
+
+        { name: 'More Information ', value: 'For more **Information** please check out the `!helpinfo` Command'},
+    )
+    .setTimestamp() 
+    .setThumbnail(user.user.displayAvatarURL())
+    .setImage("https://images-ext-2.discordapp.net/external/sskROCVWvdzH22FvhDNg5dA7sNf467gFGmpCKR14yoU/https/media.discordapp.net/attachments/888599673550536797/891418141907890196/tumblr_1c5ad679719c1993e4664b6e7616ec01_445c52fc_500.gif?width=400&height=231")
 
 
      const collector = message.channel.createMessageComponentCollector({
@@ -120,7 +176,6 @@ module.exports = new Command({
 
             collected.reply({embeds:[embed4], ephemeral:true})
         }
-
      })
 
     }
