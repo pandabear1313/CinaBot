af16a7776997efb63a44bf72e5f9804e0d40580b
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-26 15:57:54 -0400

    pushed for Twigs

---
+++ src/Commands/Animeqoute.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+const Command = require("../Structures/Command");
+
+const Discord = require("discord.js");
+
+module.exports = new Command({
+  name: "animequote",
+  description: "",
+  permission: "SEND_MESSAGES",
+  async run(message, args, client) {
+    const url = "https://animechan.vercel.app/api/random";
+
+    axios.get(url).then((res) => {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Anime Quote`)
+        .addFields(
+            { name: 'Anime', value: res.data.character},
+            { name: 'Character', value: res.data.anime },
+            { name: 'Quote', value: res.data.quote },
+        )
+
+        .setTimestamp()
+        .setThumbnail(user.user.displayAvatarURL())
+        .setColor("#ff8800")
+         
+      message.channel.send({ embeds: [embed] });
+    });
+  },
+});
