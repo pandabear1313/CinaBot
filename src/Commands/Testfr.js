7c4f868a430046817b0f8ef128737e85f98db52d
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-09-27 11:25:57 -0400

    Co-authored-by: Justinnnp <Justinnnp@users.noreply.github.com>

--- src/Commands/Testfr.js
+++ src/Commands/Testfr.js
@@ -13,68 +13,68 @@ module.exports = new Command({
 	async run(message, args, client) {
 
         let reminder = args.slice(2).join(' ');
-        let time = args[1]
+        let time = args[1];
 
         let user =
         message.mentions.members.first() ||
         message.guild.members.cache.get(args[0]) ||
         message.member;
 
-    const noDurationEmbed = new Discord.MessageEmbed();
+        const noDurationEmbed = new Discord.MessageEmbed();
 
-    noDurationEmbed
+        noDurationEmbed
 
-    .setColor("DARK_RED")
-    .setTitle("Error")
-    .setDescription("Please state a Duration for the Reminder!")
-    .setFooter(`${user.user.username} `, user.user.displayAvatarURL())
-    .setTimestamp()
+        .setColor("DARK_RED")
+        .setTitle("Error")
+        .setDescription("Please state a Duration for the Reminder!")
+        .setFooter(`${user.user.username} `, user.user.displayAvatarURL())
+        .setTimestamp()
 
-    if(!time) return message.channel.send({ embeds: [noDurationEmbed]});
+        if(!time) return message.channel.send({ embeds: [noDurationEmbed]});
 
-    const noReminderEmbed = new Discord.MessageEmbed();
+        const noReminderEmbed = new Discord.MessageEmbed();
 
-    noReminderEmbed
+        noReminderEmbed
 
-    .setColor("DARK_RED")
-    .setTitle("Error")
-    .setDescription("Please state the reminder!")
-    .setFooter(`${user.user.username} `, user.user.displayAvatarURL())
-    .setTimestamp()
+        .setColor("DARK_RED")
+        .setTitle("Error")
+        .setDescription("Please state the reminder!")
+        .setFooter(`${user.user.username} `, user.user.displayAvatarURL())
+        .setTimestamp()
 
-    if(!reminder) return message.channel.send({ embeds: [noReminderEmbed]});
+        if(!reminder) return message.channel.send({ embeds: [noReminderEmbed]});
 
-    const reminderSetEmbed = new Discord.MessageEmbed()
+        const reminderSetEmbed = new Discord.MessageEmbed()
 
-    reminderSetEmbed
+        reminderSetEmbed
 
-    .setColor("BLURPLE")
-    .setAuthor('Reminder was Set!', message.author.displayAvatarURL())
-    .setDescription(`Successfully set ${message.author.username}'s reminder!`)
-    .addField('Remind In', `${time}`)
-    .addField('Reminder', `${reminder}`)
-    .setFooter(`${user.user.username} `, user.user.displayAvatarURL())
-    .setTimestamp()
-
-    message.channel.send({ embeds: [reminderSetEmbed]});
-
-    setTimeout(async function () {
-
-        message.channel.send(`<@${message.author.id}>, here is your reminder!`);
-
-        const reminderAlertEmbed = new Discord.MessageEmbed()
-
-        reminderAlertEmbed
-    
         .setColor("BLURPLE")
-        .setAuthor('Reminder Alert!', message.author.displayAvatarURL())
+        .setAuthor('Reminder was Set!', message.author.displayAvatarURL())
+        .setDescription(`Successfully set ${message.author.username}'s reminder!`)
+        .addField('Remind In', `${time}`)
         .addField('Reminder', `${reminder}`)
         .setFooter(`${user.user.username} `, user.user.displayAvatarURL())
         .setTimestamp()
 
-        message.channel.send({ embeds: [reminderAlertEmbed]});
+        message.channel.send({ embeds: [reminderSetEmbed]});
 
-    }, ms (time));
+        setTimeout(async () => {
+
+            message.channel.send(`<@${message.author.id}>, here is your reminder!`);
+
+            const reminderAlertEmbed = new Discord.MessageEmbed()
+
+            reminderAlertEmbed
+        
+            .setColor("BLURPLE")
+            .setAuthor('Reminder Alert!', message.author.displayAvatarURL())
+            .addField('Reminder', `${reminder}`)
+            .setFooter(`${user.user.username} `, user.user.displayAvatarURL())
+            .setTimestamp()
+
+            message.channel.send({ embeds: [reminderAlertEmbed]});
+
+        }, ms(time));
 
     },
 });
