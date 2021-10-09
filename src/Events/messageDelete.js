fffec47268fe1def266244793de8c09ebdcbdc7c
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-10-08 23:55:35 -0400

    Update

---
+++ src/Events/messageDelete.js
@@ -0,0 +1,48 @@
+/** @format */
+
+const Event = require("../Structures/Event.js");
+
+
+const Discord = require('discord.js');
+
+module.exports = new Event("messageDelete", (client, message) => {
+
+    let user =
+    message.mentions.members.first() ||
+    message.member;
+
+
+  const logChannel = message.guild.channels.cache.find(chnl => chnl.name === "mod-logs");
+  if (!logChannel) return;
+
+  try {
+    if (message.member.bot) return;
+    if (!message.member.permissions.has("ADMINISTRATOR")) return;
+
+    if (message.content.startsWith("https://")) return; 
+
+    const messageDelete = new Discord.MessageEmbed()
+
+  .setAuthor(`${user.user.tag} `, user.user.displayAvatarURL())
+  .setColor("RANDOM")
+  .setTitle("Message Was Deleted")
+  .setDescription(`**Channel**\n${message.channel}\n\n**Username**\n${message.author}\n\n**Message**\n${message}`)
+  .setTimestamp()
+  .setThumbnail()
+  
+  logChannel.send({ embeds: [messageDelete] });
+
+} catch (err) {
+
+    const embed = new Discord.MessageEmbed()
+     .setColor("#FF0000")
+     .setTitle("Error!")
+     .setDescription("**Error Code:** *" + err + "*")
+     .setTimestamp();
+     logChannel.send({ embeds: [embed] });
+
+}
+
+})
+
+//.setDescription(`**Message**\n${message}\n\n**Channel**\n${message.channel}\n\n**Username**\n${message.author}`)
