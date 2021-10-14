f2964c5a2770041cfe55071fcce5f2530d97fd2c
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-10-13 22:50:58 -0400

    Added a return message if a user had their dms off credit goes to Justin

--- src/Commands/DmCommand.js
+++ src/Commands/DmCommand.js
@@ -3,24 +3,23 @@
 const Command = require("../Structures/Command.js");
 
 module.exports = new Command({
-	name: "dm",
-	description: "send a direct message to a user",
-	permission: "SEND_MESSAGES",
-	async run(message, args, client) {
+    name: "dm",
+    description: "send a direct message to a user",
+    permission: "SEND_MESSAGES",
+    async run(message, args, client) {
 
-let user = message.mentions.users.first();
-let astra = args.splice(1).join(' ');
-if(!user){
-return message.channel.send("please mention someone");
-};
-if(!astra){
-return message.channel.send("can't send an empty message");
-};
-user.send(astra);
+        let user = message.mentions.users.first();
+        let astra = args.splice(1).join(' ');
+        if (!user) return message.channel.send("please mention someone");
 
-message.delete()
+        if (!astra) return message.channel.send("can't send an empty message");
+
+        await user.send(astra).catch(err => {
+			message.channel.send(`I can't send a message to this user, <@${user.id}> try turning your dms on`); //  this works xD
+		});
+
+        message.delete()
 
     }
 });
 
-// got to redo
