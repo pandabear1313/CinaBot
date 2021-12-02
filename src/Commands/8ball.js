b83f0c40310df99c5662059b783087c977af4db2
pandabear1313 <47474662+pandabear1313@users.noreply.github.com>
2021-12-02 10:17:54 -0400

    Update 8ball.js
    reminder to update this code and add a few more cooler stuff to the output

--- src/Commands/8ball.js
+++ src/Commands/8ball.js
@@ -28,11 +28,13 @@ module.exports = new Command({
                 value: '8ball Command generally predicts ones Future Events '
             }, {
                 name: 'Command Usage',
-                value: 'Example  `!8ball` Does my gf loves me?'
+                value: 'Example  `!8ball` Should I Play Minecraft Today!'
             })
             .setTimestamp()
 
         const sayContent = args.slice(1).join(" ");
+        
+        // reminder to redo this file
 
         if (!sayContent) return message.channel.send({embeds: [usageEmbed]});
 
